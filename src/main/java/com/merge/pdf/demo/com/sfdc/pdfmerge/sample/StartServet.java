c2c8397b89d9bf8d8a153836b5001677535fee60
nagesingh@deloitte.com <nagesingh@deloitte.com>
2018-05-29 16:44:19 +0530

    .

--- src/main/java/com/merge/pdf/demo/com/sfdc/pdfmerge/sample/StartServet.java
+++ src/main/java/com/merge/pdf/demo/com/sfdc/pdfmerge/sample/StartServet.java
@@ -40,104 +40,107 @@ public class StartServet extends HttpServlet {
 	public static final String INSTANCE_URL = "INSTANCE_URL";
 	String oauthURL = "";
 
-	
+
 	private static final Logger log = Logger.getLogger(StartServet.class.getName());
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String sfdcInstance = "https://ap1.salesforce.com";
-		
+		String sfdcInstance = "https://cs91.salesforce.com";
+
 		String accessToken = (String) request.getSession().getAttribute(ACCESS_TOKEN);
 		String code = request.getParameter("code");
-		
-		String consumerKey = "3MVG9Y6d_Btp4xp7hZ1hn287sIMQ56.b_dOEBDrKHRbtwByFvFJz0A4ETmj_TbTuu4CsCkknL3ZgoWIzUTBnh";
-		String consumerSecret = "4972968186270851852";
-		String redirectUri = "https://localhost:8443/PDFMerge_J2EE/TestOAuth?isCallBack=1";
-		
+		System.out.println("code : "+request.getParameter("code"));
+
+		String consumerKey = "3MVG9d3kx8wbPieHjOp9VVrZ9A5CJRa582aZvgXcfAQf0doVuX.CB2JTws_oBUvx.uyBlsHeE77MxlUpOLwEm";
+		String consumerSecret = "3139176758014005887";
+		String redirectUri = "https://pdfmergedemo.herokuapp.com/TestOAuth?isCallBack=1";
+
 		oauthURL = sfdcInstance + "/services/oauth2/authorize?response_type=code&client_id=" + consumerKey + "&redirect_uri="
 				+ URLEncoder.encode(redirectUri, "UTF-8");
-		
+
 		if (accessToken == null) {
-			
+
 			try {
 				String isCallBack = request.getParameter("isCallBack");
-					log.info("Starting OAuth");
-					log.info("Is CallBack = "+isCallBack);
+				log.info("Starting OAuth");
+				log.info("Is CallBack = "+isCallBack);
+				System.out.println("Inside Try block : Starting Oauth :");
+				System.out.println("Is CallBack = "+isCallBack);
+
+				/**
+				 * Initiate HTTP request to get the Authorization code
+				 */
+				DefaultHttpClient httpclient = new DefaultHttpClient();
+				HttpPost httpPost = new HttpPost(oauthURL);
+				HttpResponse response1 = httpclient.execute(httpPost);
+
+				/**
+				 * If HTTP status code is 302, means Salesforce trying to redirect to authorize the Local System
+				 */
+				if(response1.getStatusLine().getStatusCode() == 302 && isCallBack == null)
+				{
+					httpclient.getConnectionManager().shutdown();
+					log.info("Got 302 request from Salesforce so redirect it");
+					response.sendRedirect(oauthURL);
+					return;
+				}
+				else
+				{
+					log.info("Got Callback, Now get the Access Token");
+
+					httpclient = new DefaultHttpClient();
+					String tokenUrl = sfdcInstance + "/services/oauth2/token";
+
+					log.info("Code - "+code);
+
+					List<NameValuePair> qparams = new ArrayList<NameValuePair>();
+					qparams.add(new BasicNameValuePair("code", code));
+					qparams.add(new BasicNameValuePair("grant_type", "authorization_code"));
+					qparams.add(new BasicNameValuePair("client_id", consumerKey));
+					qparams.add(new BasicNameValuePair("client_secret", consumerSecret));
+					qparams.add(new BasicNameValuePair("redirect_uri", redirectUri));
+
+					HttpPost httpPost2 = new HttpPost(tokenUrl);
+					httpPost2.setEntity(new UrlEncodedFormEntity(qparams));
 
 					/**
-					 * Initiate HTTP request to get the Authorization code
+					 * Always set this Header to explicitly to get Authrization code else following error will be raised
+					 * unsupported_grant_type
 					 */
-					DefaultHttpClient httpclient = new DefaultHttpClient();	
-					HttpPost httpPost = new HttpPost(oauthURL);					
-					HttpResponse response1 = httpclient.execute(httpPost);
-					
-					/**
-					 * If HTTP status code is 302, means Salesforce trying to redirect to authorize the Local System
-					 */
-					if(response1.getStatusLine().getStatusCode() == 302 && isCallBack == null)
-					{
-						httpclient.getConnectionManager().shutdown();
-						log.info("Got 302 request from Salesforce so redirect it");
-						response.sendRedirect(oauthURL);
-						return;
-					}
-					else
-					{	
-						log.info("Got Callback, Now get the Access Token");
-						
-						httpclient = new DefaultHttpClient();						
-						String tokenUrl = sfdcInstance + "/services/oauth2/token";
-						
-						log.info("Code - "+code);
-						
-						List<NameValuePair> qparams = new ArrayList<NameValuePair>();
-						qparams.add(new BasicNameValuePair("code", code));
-						qparams.add(new BasicNameValuePair("grant_type", "authorization_code"));
-						qparams.add(new BasicNameValuePair("client_id", consumerKey));
-						qparams.add(new BasicNameValuePair("client_secret", consumerSecret));
-						qparams.add(new BasicNameValuePair("redirect_uri", redirectUri));
-						
-						HttpPost httpPost2 = new HttpPost(tokenUrl);
-						httpPost2.setEntity(new UrlEncodedFormEntity(qparams));
-						
-						/**
-						 * Always set this Header to explicitly to get Authrization code else following error will be raised
-						 * unsupported_grant_type
-						 */
-						httpPost2.setHeader("Content-Type", "application/x-www-form-urlencoded");
-						
-						HttpResponse response2 = httpclient.execute(httpPost2); 
-						
-						HttpEntity entity2 = response2.getEntity();
-						System.out.println(entity2.getContent().toString());
-						
-						JSONObject authResponse = new JSONObject(new JSONTokener(new InputStreamReader(entity2.getContent())));
-						System.out.println(authResponse.toString());
-						accessToken = authResponse.getString("access_token");
-						String instanceUrl = authResponse.getString("instance_url");
-						
-						request.getSession().setAttribute(ACCESS_TOKEN, accessToken);
-		
-						// We also get the instance URL from the OAuth response, so set it
-						// in the session too
-						request.getSession().setAttribute(INSTANCE_URL, instanceUrl);
-						
-						log.info("Response is : "+authResponse);
-						log.info("Got access token: " + accessToken);
-						
+					httpPost2.setHeader("Content-Type", "application/x-www-form-urlencoded");
+
+					HttpResponse response2 = httpclient.execute(httpPost2);
+
+					HttpEntity entity2 = response2.getEntity();
+					System.out.println(entity2.getContent().toString());
+
+					JSONObject authResponse = new JSONObject(new JSONTokener(new InputStreamReader(entity2.getContent())));
+					System.out.println(authResponse.toString());
+					accessToken = authResponse.getString("access_token");
+					String instanceUrl = authResponse.getString("instance_url");
+
+					request.getSession().setAttribute(ACCESS_TOKEN, accessToken);
+
+					// We also get the instance URL from the OAuth response, so set it
+					// in the session too
+					request.getSession().setAttribute(INSTANCE_URL, instanceUrl);
+
+					log.info("Response is : "+authResponse);
+					log.info("Got access token: " + accessToken);
+
 						/*
 						PrintWriter out = response.getWriter();
 						out.println("Auth successful - got callback");
 						*/
-						String pdfUrl = "/PDFMerge_J2EE/pdfmerge?ids=00P900000038KTm&parentId=00690000007GLNj&mergedDocName=MergedDoc.pdf";
-						response.sendRedirect(pdfUrl);
+					String pdfUrl = "/pdfmerge?ids=0692F000000FPHbQAO&parentId=a0Z2F000000eFEVUA2&mergedDocName=MergedDoc.pdf";
+					response.sendRedirect(pdfUrl);
 				}
 
 			} catch (Exception e) {
-					e.printStackTrace();
+				e.printStackTrace();
 			} finally {
 
 			}
-			
+
 		}
 	}
 
