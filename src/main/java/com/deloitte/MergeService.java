3d209d18e3bfcb81d54b353d3c9bc40c9db6a6fb
Manikanta-Padala <35959192+Manikanta-Padala@users.noreply.github.com>
2018-05-30 20:37:59 +0530

    Update MergeService.java

--- src/main/java/com/deloitte/MergeService.java
+++ src/main/java/com/deloitte/MergeService.java
@@ -18,7 +18,7 @@ public class MergeService {
 	   public static String mergeUsers(@RequestParam("file1Id") String file1Id,
 			   @RequestParam("file2Id") String file2Id,
 			   @RequestParam("parentId") String parentId){
-		   Gson gson = new GsonBuilder().disableHtmlEscaping().create(); 
+		   Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 		   
 		   return gson.toJson("SUCCESS");
 		   //MergeAndUploadPDF.mergeanduploadPDF(file1Id, file2Id, parentId);; 
