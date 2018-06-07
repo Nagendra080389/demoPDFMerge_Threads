fd2f567117fae46310fb958d6377e732b5fc8fa1
nagesingh@deloitte.com <nagesingh@deloitte.com>
2018-06-07 15:54:02 +0530

    setServiceEndpoint

--- src/main/java/com/deloitte/MergeService.java
+++ src/main/java/com/deloitte/MergeService.java
@@ -1,4 +1,5 @@
 package com.deloitte;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,10 +10,13 @@ import javax.ws.rs.core.MediaType;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.logging.Logger;
+
 @RestController
 public class MergeService {
 
 
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(MergeService.class);
     @RequestMapping(value = "/MergeNSplitService/merge", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
     public static String mergeUsers(@RequestParam("file1Id") String file1Id,
                                     @RequestParam("file2Id") String file2Id,
@@ -21,6 +25,12 @@ public class MergeService {
                                     @RequestParam("instanceURL") String instanceURL,
                                     @RequestParam("useSoap")boolean useSoap) {
         Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+        LOGGER.info("file1Id -> "+file1Id);
+        LOGGER.info("file2Id -> "+file2Id);
+        LOGGER.info("parentId -> "+parentId);
+        LOGGER.info("accessToken -> "+accessToken);
+        LOGGER.info("instanceURL -> "+instanceURL);
+        LOGGER.info("useSoap -> "+useSoap);
         MergeAndUploadPDF.mergeanduploadPDF(file1Id, file2Id, parentId,accessToken,instanceURL,useSoap );
         return gson.toJson("Merge PDF SUCCESS");
 
